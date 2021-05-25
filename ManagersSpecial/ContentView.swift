4c227d0b4a8e88871e9182dd1bb85e158616c46e
Todd Thomas <toddthomas@acm.org>
2021-05-24 21:51:56 -0600

    Initial Commit

---
+++ ManagersSpecial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ManagersSpecial
+//
+//  Created by Todd Thomas on 5/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ManagersSpecial/ManagersSpecialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagersSpecialApp.swift
+//  ManagersSpecial
+//
+//  Created by Todd Thomas on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ManagersSpecialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
